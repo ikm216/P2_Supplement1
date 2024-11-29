295721a1df0e62ca2eaa18b4f1d887227a5717f2
ikm216 <ikmclassof22@gmail.com>
2024-11-28 23:30:04 -0500

    feat: added the classes Node and getNode to insert the node

---
+++ Node.java
@@ -0,0 +1,13 @@
+public class Node{
+   private int nodeId, data;
+   private Node left, right;
+
+   public Node(int nodeId, int data){
+    this.nodeId = nodeId;
+    this.data = data;
+   }
+
+   public int getNode(){
+    return data;
+   }
+}
--- test.java
+++
@@ -1,5 +0,0 @@
-public class test{
-    public String says(){
-        return "Test Test";
-    }
-}
